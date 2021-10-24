569f8abc45007fcbbd8d03748cda4d905c36cd08
Astrid Alaniz <alanizastrid@gmail.com>
2021-10-24 13:40:34 -0500

    refactor
    refactor update_all_render_settings for readability

--- utilities.py
+++ utilities.py
@@ -1,10 +1,10 @@
 import bpy
 
-def update_all_render_settings(x, y, p, s):
+def update_all_render_settings(x_res=1920, y_res=1080, scale=100, samples=2**4):
     for scene in bpy.data.scenes:
-        scene.render.resolution_x = x
-        scene.render.resolution_y = y
-        scene.render.resolution_percentage = p
-        scene.eevee.taa_render_samples = s
+        scene.render.resolution_x = x_res
+        scene.render.resolution_y = y_res
+        scene.render.resolution_percentage = scale
+        scene.eevee.taa_render_samples = samples
         
-update_all_render_settings(2160,2160, 25, 1)
+update_all_render_settings(1920,1080, 100, 2**4)
